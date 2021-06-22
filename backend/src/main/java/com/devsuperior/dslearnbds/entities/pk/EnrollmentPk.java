34dcedf462eefb235da89951ebc511457fce88eb
Vitor Battestin <vitor.battestin@gmail.com>
2021-06-22 14:51:41 -0300

    Enrollment, EnrollmentPk

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/pk/EnrollmentPk.java
@@ -0,0 +1,59 @@
+package com.devsuperior.dslearnbds.entities.pk;
+
+import com.devsuperior.dslearnbds.entities.Offer;
+import com.devsuperior.dslearnbds.entities.User;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class EnrollmentPk implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    public EnrollmentPk(){
+    }
+
+    public EnrollmentPk(User user, Offer offer) {
+        this.user = user;
+        this.offer = offer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EnrollmentPk that = (EnrollmentPk) o;
+        return user.equals(that.user) && offer.equals(that.offer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, offer);
+    }
+}
